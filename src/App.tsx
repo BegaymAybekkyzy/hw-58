baa41c279c876fd291cc1162f9da7783ba20abeb
Aybek kyzy Begaym <ajbekkyzybegajym1@gmail.com>
2025-01-20 17:24:17 +0600

    Уровень 1: Реализовано дополнительное задание - передача массива конфигураций кнопок

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import Modal from './components/UI/Modal/Modal.tsx';
-import { useState } from 'react';
+import Modal from "./components/UI/Modal/Modal.tsx";
+import { useState } from "react";
+import { IButton } from "./types";
 
 const App = () => {
   const [showModal, setShowModal] = useState<boolean>(false);
 
+  const buttons: IButton[] = [
+    {
+      type: "primary",
+      label: "Continue",
+      onClick: () => alert("clicked continue"),
+    },
+
+    {
+      type: "danger",
+      label: "Close",
+      onClick: () => closeOrOpenModal(),
+    },
+  ];
+
   const closeOrOpenModal = () => {
-    setShowModal(prevState => !prevState);
+    setShowModal((prevState) => !prevState);
   };
 
   return (
     <>
-      <Modal show={showModal} onClose={closeOrOpenModal}>
-        <div className="p-3">This is modal content</div>
+      <Modal
+        show={showModal}
+        onClose={closeOrOpenModal}
+        buttons={buttons}
+        title="Some kinda modal title"
+      >
+        <div>This is modal content</div>
       </Modal>
 
       <div className="container">
         <div className="p-3">
           <p>lvl 1</p>
-          <button className="btn btn-dark" onClick={closeOrOpenModal}>Open modal</button>
+          <button className="btn btn-dark" onClick={closeOrOpenModal}>
+            Open modal
+          </button>
         </div>
       </div>
-
     </>
   );
 };
--- src/components/UI/Modal/Modal.tsx
+++ src/components/UI/Modal/Modal.tsx
@@ -1,31 +1,45 @@
-import BackDrop from '../BackDrop/BackDrop.tsx';
-import React, { ReactNode } from 'react';
+import BackDrop from "../BackDrop/BackDrop.tsx";
+import React, { ReactNode } from "react";
+import { IButton } from "../../../types";
 
 interface Props {
-  show?: boolean;
+  show: boolean;
   onClose: () => void;
-  title?: string;
+  title: string;
   children?: ReactNode;
+  buttons?: IButton[];
 }
 
 const Modal: React.FC<Props> = ({
-    show = true,
-    onClose, title = 'Some kinda modal title',
-    children
-  }
-) => {
-
+  show = true,
+  onClose,
+  title = "Modal title",
+  children,
+  buttons,
+}) => {
   return (
     <>
-      <BackDrop show={show}/>
-      <div className="modal show" style={{display: show ? 'block' : 'none'}}>
+      <BackDrop show={show} />
+      <div className="modal show" style={{ display: show ? "block" : "none" }}>
         <div className="modal-dialog">
           <div className="modal-content">
             <div className="modal-header">
               <h1 className="modal-title fs-5">{title}</h1>
               <button className="btn-close" onClick={onClose}></button>
             </div>
-            {children}
+            <div className="modal-body">{children}</div>
+            <div className="modal-footer">
+              {buttons
+                ? buttons.map((btn) => (
+                    <button
+                      className={`btn btn-${btn.type}`}
+                      onClick={() => btn.onClick()}
+                    >
+                      {btn.label}
+                    </button>
+                  ))
+                : null}
+            </div>
           </div>
         </div>
       </div>
